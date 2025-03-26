512eaed188a7c9253000b68eaab5803403198498
bestwonderdev <bestwonderdev@gmail.com>
2025-03-27 00:59:50 +0100

    integration test modified

--- integration_inflation_test.sh
+++ integration_inflation_test.sh
@@ -3,10 +3,10 @@
 set -ue
 
 #apt update list
-#apt install ginkgo
-#go install github.com/onsi/ginkgo/v2/ginkgo@latest
-#go install github.com/onsi/gomega/...@latest
-#export PATH=$PATH:$(go env GOPATH)/bin
+apt install ginkgo
+go install github.com/onsi/ginkgo/v2/ginkgo@latest
+go install github.com/onsi/gomega/...@latest
+export PATH=$PATH:$(go env GOPATH)/bin
 pwd
 cd ./x/inflation/keeper
 pwd
